4ed0ffb8398f14ce04482032467ae54ef476fc9b
trevorswann <tswann2@ycp.edu>
2021-10-31 12:46:18 -0400

    refactoring

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "collatzConjecture.h"
-
-int main() {
-    std::cout << "Running Collatz Conjecture Program!" << std::endl;
-
-    __attribute__((unused)) collatzConjecture myCollatz;
-
-    return 0;
-}
